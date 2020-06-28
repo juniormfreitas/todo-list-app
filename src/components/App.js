d3a69f5df91545389c7ddaf58f7550568664493a
J. de Freitas <juniormfreitas@gmail.com>
2020-06-27 18:42:17 -0600

    Installing Bootstrap V5-alpha

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "../scss/main.scss";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello World - All</div>;
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-md-center">Todo List App</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
