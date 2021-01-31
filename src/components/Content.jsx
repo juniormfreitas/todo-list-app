93e7ddd134dac46eecb7bc506b1ef616d4a85929
J. de Freitas <juniormfreitas@gmail.com>
2021-01-31 17:55:17 -0600

    Remove console.log

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { View } from '@adobe/react-spectrum';
 
-const Content = () => {
-  console.log('test');
-
-  return (
-    <>
-      <View backgroundColor="purple-600" gridArea="content" />
-    </>
-  );
-};
+const Content = () => (
+  <>
+    <View backgroundColor="purple-600" gridArea="content" />
+  </>
+);
 
 export default Content;
